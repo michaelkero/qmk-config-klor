538b79dcaf94c3e15826eee602f25fb9d958febf
Michael Ker0 <michael.kero@ambr.net>
2025-06-24 11:02:43 -0400

    comments

--- klor/keymaps/michaelkero/keymap.c
+++ klor/keymaps/michaelkero/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
               KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                          KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     ALT_GRV,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                          KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  GUI_QUT,
-    KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,   _______,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
+    CTL_ENT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_MUTE,   _______,  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  SFT_SPC,
                                   KC_LGUI,  LOWER,    SFT_SPC,  KC_BSPC,   KC_ESC,   CTL_ENT,  RAISE,    KC_LALT
 ),
 
@@ -123,8 +123,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  └─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┘ */ 
    [_LOWER] = LAYOUT_polydactyl(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷         ╷
-              _______,  _______,  KC_CIRC,  KC_PAST,  KC_PPLS,                       KC_HASH,  KC_LCBR,  KC_RCBR,  KC_LABK,  KC_RABK,
-    _______,  KC_PIPE,  KC_AMPR,  KC_EXLM,  KC_EQL,   KC_UNDS,                       KC_DLR,   KC_LPRN,  KC_RPRN,  _______,  _______,  _______,
+              KC_GRAVE, _______,  KC_CIRC,  KC_PAST,  KC_PPLS,                       KC_HASH,  KC_LCBR,  KC_RCBR,  KC_LABK,  KC_RABK,
+    _______,  KC_PIPE,  KC_AMPR,  KC_EXLM,  KC_EQL,   KC_UNDS,                       KC_DLR,   KC_LPRN,  KC_RPRN,  _______,  KC_QUOT,  _______,
     _______,  _______,  _______,  KC_PERC,  KC_SLSH,  KC_MINS,  _______,   _______,  KC_AT,    KC_LBRC,  KC_RBRC,  _______,  KC_BSLS,  _______,
                                   _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______
 ),
@@ -136,11 +136,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    │ r a i s e                                                 │
    └───────────────────────────────────────────────────────────┘
              ┌─────────┬─────────┬─────────┬─────────┬─────────┐                    ┌─────────┬─────────┬─────────┬─────────┬─────────┐
-             │    !    │    @    │    #    │    $    │    %    │ ╭╮╭╮╭╮╭╮╭╮╭╮╭╮╭╮╭╮ │    ^    │    &    │         │    °    │    /    │   
+             │    1    │    2    │    3    │    4    │    5    │ ╭╮╭╮╭╮╭╮╭╮╭╮╭╮╭╮╭╮ │    6    │    7    │    8    │    9    │    0    │   
    ┌─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ │╰╯╰╯╰╯╰╯╰╯╰╯╰╯╰╯│ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┐
    │         │         │         │         │         │         ├─╯                ╰─┤         │         │         │         │         │         │
    ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤╭────────╮╭────────╮├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-   │         │         │         │         │         │         ││  MUTE  ││PLY/PSE ││         │         │         │         │         │         │
+   │         │         │         │         │         │         ││  MUTE  ││PLY/PSE ││   LEFT  │   DOWN  │    UP   │  RIGHT  │         │         │
    └─────────┴─────────┴─────────┼─────────┼─────────┼─────────┼╰────────╯╰────────╯┼─────────┼─────────┼─────────┼─────────┴─────────┴─────────┘
                                  │         │ ADJUST  │    ▼    │    ▼    ││    ▼    │    ▼    │    ▼    │    ▼    │  
                                  └─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┘ */ 
